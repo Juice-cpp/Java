e947f0717c5e981c4c60c71a12ec3e4867d7dfee
Juice <julio.cetaranto@gmail.com>
2024-08-05 20:10:50 -0300

    Add files via upload

---
+++ Ex 1 Podrao/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int hora1 = 10, hora2 = 4, hora3 = 2;
+        int vendas = hora1 + hora2 + hora3;
+        double media = vendas/(3*1.0);
+        System.out.println("Total: " + vendas);
+        System.out.println("Media: " + media);
+    }
+}
