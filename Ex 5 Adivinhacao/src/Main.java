4746b7b7952735d3a3e76cbfc4c54c440c3cbc30
Juice <julio.cetaranto@gmail.com>
2024-08-08 22:13:02 -0300

    Add files via upload

---
+++ Ex 5 Adivinhacao/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int nRand = rand.nextInt(10) + 1;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Adivinhe o número de 1 a 10: ");
+        while(true) {
+            int nAdv = entrada.nextInt();
+            if (nAdv == nRand) {
+                System.out.println("Acertou!!");
+                break;
+            } else {
+                System.out.println("Errou.");
+            }
+        }
+    }
+}
