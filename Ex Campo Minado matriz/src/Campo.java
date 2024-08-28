6b50a94b7f279e7acb11777a9559c307389929b4
Juice-cpp <julio.cetaranto@gmail.com>
2024-08-28 00:54:13 -0300

    Finalizado

--- Ex Campo Minado matriz/src/Campo.java
+++ Ex Campo Minado matriz/src/Campo.java
@@ -2,14 +2,17 @@ import java.util.Random;
 
 public class Campo {
     Random rand = new Random();
-    boolean[][] mapa;
-    mapa = new boolean[2][2];
-    boolean continuar;
+    boolean[][] mapa = new boolean[2][2];
+    int continuar = 1;
+    int localSeguro = 0;
 
     void start() {
         for (int i = 0; i < mapa.length; i++) {
             for (int j = 0; j < mapa[i].length; j++) {
                 mapa[i][j] = rand.nextBoolean();
+                if(mapa[i][j]) {
+                    localSeguro += 1;
+                }
             }
         }
     }
@@ -20,9 +23,12 @@ public class Campo {
             continuar = 0;
         }
         else {
+            localSeguro -= 1;
             System.out.println("Está seguro!!");
-            continuar = 1;
+            if(localSeguro == 0) {
+                System.out.println("Parabéns! Jogo finalizado.");
+                continuar = 0;
+            }
         }
     }
-
 }
--- Ex Campo Minado matriz/src/Main.java
+++ Ex Campo Minado matriz/src/Main.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 public class Main {
-    private static java.lang.Object continuar;
-
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         Campo campo = new Campo();
 
+        System.out.println("Este campo tem um tamanho 2x2.");
         System.out.println("Deseja começar? (1) Sim  (0) Não");
-        boolean começar = entrada.nextBoolean();
-        int coordX = entrada.nextInt();
-        int coordY = entrada.nextInt();
-        while(começar) {
+        int comecar = entrada.nextInt();
+
+        if(comecar == 1) {
             campo.start();
-            
+        }
+        while(comecar == 1) {
+            System.out.print("Insira uma coordenada: ");
             int coordX = entrada.nextInt();
             int coordY = entrada.nextInt();
-            campo.verificar();
-            começar = campo.continuar;
+            campo.verificar(coordX, coordY);
+            comecar = campo.continuar;
         }
     }
 }
