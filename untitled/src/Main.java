0ba6a1faa797aacb36195704984878193b5690e6
Juice-cpp <julio.cetaranto@gmail.com>
2024-09-11 02:28:21 -0300

    Testando o git no note

---
+++ untitled/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] pos = new int[10];
+
+        for(int i = 0; i < pos.length; i++) {
+            pos[i] = i;
+            System.out.println("Posição: " + pos[i]);
+        }
+    }
+}
